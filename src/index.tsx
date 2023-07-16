5de15e8342c1b522eedfa79bbc38b618cacac136
AlexPoloz <pol2shkan4ik@gmail.com>
2023-07-16 20:34:36 +0300

    add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import App from './app/App';
 import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider';
 
@@ -9,8 +10,11 @@ const root = createRoot(document.getElementById('root')!);
 
 root.render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
-    </BrowserRouter>,
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
+    </BrowserRouter>
+  ,
 );
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+const reloadPage = () => {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+};
+
+export const PageError = ({ className }: PageErrorProps) => {
+  const { t } = useTranslation();
+  return (
+      <div className={classNames(cls.PageError, {}, [className])}>
+          <p>{t('Произошла непредвиденная ошибка')}</p>
+          <Button onClick={reloadPage}>{t('Обновить страницу')}</Button>
+      </div>
+  );
+};
